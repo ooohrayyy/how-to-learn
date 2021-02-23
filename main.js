db0c2ffe13ef87102498d0eab8f86221ef711c8d
Boris Nikitashov <ooohrayyy@gmail.com>
2021-02-23 16:04:24 +0300

    Updates

---
+++ main.js
@@ -0,0 +1,1 @@
+(()=>{"use strict";const e=document.querySelector(".suggestions__form"),t=document.querySelector(".suggestions__thanks");e.addEventListener("submit",(function(s){s.preventDefault(),e.style.visibility="hidden",setTimeout((()=>{e.style.display="none"}),300),t.style.display="block"}))})();
