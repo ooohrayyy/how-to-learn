a3a400ac88aa89270d9368dadd92627e43ebb2cc
Boris Nikitashov <ooohrayyy@gmail.com>
2021-02-23 16:01:13 +0300

    feat: Оживить форму для предложений

--- src/index.js
+++ src/index.js
@@ -1,1 +1,20 @@
 import './pages/index.css';
+
+// * Логика поведения формы
+
+const form = document.querySelector('.suggestions__form');
+const thxMessage = document.querySelector('.suggestions__thanks');
+
+function handleFormSubmit(evt) {
+  evt.preventDefault();
+
+  form.style.visibility = 'hidden';
+
+  setTimeout(() => {
+    form.style.display = 'none';
+  }, 300);
+
+  thxMessage.style.display = 'block';
+}
+
+form.addEventListener('submit', handleFormSubmit);
